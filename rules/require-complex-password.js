72feaf8469edf2d2372e59f2161ef253591d4d77
Roman <trexbazarov@gmail.com>
2016-09-28 16:46:05 +0300

    Update require-complex-password.js

--- rules/require-complex-password.js
+++ rules/require-complex-password.js
@@ -2,6 +2,5 @@ function (ctx, cb) {
 	if (!ctx.user.password || ctx.user.password.length < 8) {
 		return cb(new UnauthorizedError('Password does not match the password policy.'));
 	};
-
 	cb(null, ctx);
 }
